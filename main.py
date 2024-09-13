f57fa293ad7b2505aecff1a26bab1effecb9be61
KKolchev <kostadin.kolchev0022@gmail.com>
2024-09-13 11:12:35 +0300

    The check for the entry level is ready and student's final results are saved in the db

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, redirect, request, flash
-import requests
+from flask import Flask, render_template, request, flash
 from werkzeug.utils import secure_filename
 from flask_mysqldb import MySQL
 import pandas as pd
@@ -40,20 +39,23 @@ def load_exam_answers(exam_csv):
     class_name = request.form.get('class_name')
     global exam_name
     cursor = mysql.connection.cursor()
-    cursor.execute('''INSERT INTO exams(exam_name, class) VALUES(%s, %s) ''', (exam_name, class_name, ))
-    cursor.execute('''SELECT id FROM exams WHERE exam_name = %s''', (exam_name, ))
+    cursor.execute('''INSERT INTO exams(exam_name, class) VALUES(%s, %s) ''', (exam_name, class_name,))
+    cursor.execute('''SELECT id FROM exams WHERE exam_name = %s''', (exam_name,))
     result = cursor.fetchall()[0][0]
     for index, curr_row in exam_answers.iterrows():
-        cursor.execute('''INSERT INTO exam_questions( exam_id, question_type, question_number) VALUES(%s, %s, %s)''',
-                       (result, curr_row['Тип въпрос'], curr_row['Въпрос'], ))
+        cursor.execute('''INSERT INTO exam_questions( exam_id, question_type, question_number) 
+                          VALUES(%s, %s, %s)''',
+                       (result, curr_row['Тип въпрос'], curr_row['Въпрос'],))
         cursor.execute('''SELECT id 
                           FROM exam_questions 
                           WHERE exam_id = %s
                           AND question_type = %s
-                          AND question_number = %s''', (result, curr_row['Тип въпрос'], curr_row['Въпрос'], ))
+                          AND question_number = %s''',
+                       (result, curr_row['Тип въпрос'], curr_row['Въпрос'],))
         question_id = cursor.fetchall()[0][0]
         cursor.execute('''INSERT INTO exams_answers(answer, exam_id, question_id, points)
-                          VALUES(%s, %s, %s, %s)''', (curr_row['Отговор'], result, question_id, curr_row['Точки']))
+                          VALUES(%s, %s, %s, %s)''',
+                       (curr_row['Отговор'], result, question_id, curr_row['Точки']))
 
     mysql.connection.commit()
     cursor.close()
@@ -87,6 +89,99 @@ def load_assessment_scale(id_of_exam):
 
     cursor.execute('INSERT INTO exam_marks_scale VALUES(%s, %s, %s, %s)',
                    (6, lower_bound_6, upper_bound_6, id_of_exam))
+
+    mysql.connection.commit()
+    cursor.close()
+
+
+def load_final_report(student_class, student_num, student_name, exam_title):
+    cursor = mysql.connection.cursor()
+    cursor.execute("""select ex.id
+                                from exams ex
+                                where ex.exam_name = %s
+                                and ex.class = %s""",
+                   (exam_title, student_class))
+    student_ex = cursor.fetchall()[0][0]
+    cursor.execute("""INSERT INTO students_results_report(student_id, exam_id, 
+                                                                  final_grade, student_name)
+                              VALUES(%s, %s, %s, %s)""",
+                   (student_num, student_ex, -1, student_name))
+
+    mysql.connection.commit()
+    cursor.close()
+
+
+def get_qst_answer_info(question_number, exam, student_class):
+    cursor = mysql.connection.cursor()
+    cursor.execute("""select ans.answer,
+                             ans.points,
+                            qst.question_type
+                      from exams_answers ans
+                      join exams ex on ex.id = ans.exam_id
+                      join exam_questions qst on qst.id = ans.question_id
+                      join students_results_report rp on rp.exam_id = ex.id
+                      where ex.exam_name = %s
+                        and ex.class = %s
+                        and qst.question_number = %s""",
+                   (exam, student_class, question_number,))
+    result = cursor.fetchall()
+    question_answer, points, question_type = result[0]
+    return question_answer, points, question_type
+
+
+def check_select_answer(question_number, exam, student_class, student_answer, subsection):
+    question_answer, points, question_type = get_qst_answer_info(question_number, exam, student_class)
+
+    question_answer = question_answer.split(';')
+
+    question_answer = [item.split('-') for item in question_answer]
+
+    for curr in question_answer:
+        new_arr = []
+        for el in curr:
+            new_el = el.strip()
+            new_arr.append(new_el)
+        if new_arr[0] == subsection and new_arr[1] != student_answer:
+            return False
+
+    return True
+
+
+def check_one_choice_answer(question_number, exam, student_class, student_answer):
+    question_answer, points, question_type = get_qst_answer_info(question_number, exam, student_class)
+
+    question_answer = question_answer.strip()
+
+    if question_answer != student_answer:
+        return False
+
+    return True
+
+
+def check_multiple_choice_answer(question_number, exam, student_class, student_answer):
+    question_answer, points, question_type = get_qst_answer_info(question_number, exam, student_class)
+
+    question_answer = question_answer.split(';')
+    question_answer = [item.strip() for item in question_answer]
+
+    if len(question_answer) != len(student_answer) or question_answer == student_answer:
+        return False
+
+    return True
+
+
+def final_mark_update(exam_title, student_num, student_name, student_class, final_points):
+    cursor = mysql.connection.cursor()
+    cursor.execute("""update students_results_report rep
+                              join exams ex on ex.id = rep.exam_id
+                              join exam_marks_scale scl on scl.exam_id = ex.id
+                              set rep.final_grade = scl.mark
+                              where ex.exam_name = %s
+                              and rep.student_id = %s
+                              and rep.student_name = %s
+                              and ex.class = %s
+                              and %s between scl.from_points and scl.to_points;""",
+                   (exam_title, student_num, student_name, student_class, final_points))
 
     mysql.connection.commit()
     cursor.close()
@@ -148,23 +243,129 @@ def entry_level_page():
         q7_stocks_list = request.form.getlist('stocks_list')
         q8_correct_word = request.form.getlist('correct_word')
         q9_call_a_friend = request.form.getlist('call_a_friend')
+        exam_title = request.form.get('entry_lvl')
 
-        cursor = mysql.connection.cursor()
-        cursor.execute("""select ex.id
-                            from exams ex
-                            where ex.exam_name = %s
-                            and ex.class = %s""",
-                       ('Входно ниво', student_class))
-        student_ex = cursor.fetchall()[0][0]
-        cursor.execute("""INSERT INTO students_results_report(student_id, exam_id, 
-                                                              final_grade, student_name)
-                          VALUES(%s, %s, %s, %s)""",
-                       (student_num, student_ex, -1, student_name))
+        load_final_report(student_class=student_class, student_num=student_num,
+                          student_name=student_name, exam_title=exam_title)
 
-        mysql.connection.commit()
-        cursor.close()
+        final_points = 0
+
+        """Проверка на въпрос 1"""
+        paper_answer_res = check_select_answer(1, exam_title,
+                                               student_class, q1_paper_answer, 'А')
+        metal_answer_res = check_select_answer(1, exam_title,
+                                               student_class, q1_metal_answer, 'Б')
+        plastic_answer_res = check_select_answer(1, exam_title,
+                                                 student_class, q1_plastic_answer, 'В')
+        wood_answer_res = check_select_answer(1, exam_title,
+                                              student_class, q1_wood_answer, 'Г')
+
+        if paper_answer_res is True \
+                and metal_answer_res is True \
+                and plastic_answer_res is True \
+                and wood_answer_res is True:
+            question_answer, points, question_type = get_qst_answer_info(1,
+                                                                         exam_title, student_class)
+            final_points += points
+
+        """Проверка на въпрос 2"""
+        cylinder_answer = check_select_answer(2, exam_title, student_class,
+                                              q2_cylinder_answer, 'а')
+        pyramid_answer = check_select_answer(2, exam_title, student_class,
+                                             q2_pyramid_answer, 'б')
+        tent_answer = check_select_answer(2, exam_title, student_class,
+                                          q2_tent_answer, 'в')
+        cube_answer = check_select_answer(2, exam_title, student_class,
+                                          q2_cube_answer, 'г')
+
+        if cylinder_answer is True \
+                and pyramid_answer is True \
+                and tent_answer is True \
+                and cube_answer is True:
+            question_answer, points, question_type = get_qst_answer_info(2, exam_title,
+                                                                         student_class)
+            final_points += points
+
+        """Проверка на въпрос 3"""
+        will_last_answer = check_one_choice_answer(3, exam_title, student_class,
+                                                   q3_will_last_answer)
+
+        if will_last_answer is True:
+            question_answer, points, question_type = get_qst_answer_info(3, exam_title,
+                                                                         student_class)
+            final_points += points
+
+        """Проверка на въпрос 4"""
+        most_resistant_answer = check_one_choice_answer(4, exam_title, student_class,
+                                                        q4_most_resistant_answer)
+
+        if most_resistant_answer is True:
+            question_answer, points, question_type = get_qst_answer_info(4, exam_title,
+                                                                         student_class)
+            final_points += points
+
+        """Проверка на въпрос 5"""
+        metal_property_answer = check_select_answer(5, exam_title, student_class,
+                                                    q5_metal_property_answer, 'А')
+        rubber_band_property_answer = check_select_answer(5, exam_title, student_class,
+                                                          q5_rubber_band_property_answer, 'Б')
+        paper_property_answer = check_select_answer(5, exam_title, student_class,
+                                                    q5_paper_property_answer, 'В')
+
+        if metal_property_answer is True \
+                and rubber_band_property_answer is True \
+                and paper_property_answer is True:
+            question_answer, points, question_type = get_qst_answer_info(5, exam_title,
+                                                                         student_class)
+            final_points += points
+
+        """Проверка на въпрос 6"""
+        drawing_instrument_answer = check_select_answer(6, exam_title, student_class,
+                                                        q6_drawing_instrument_answer, 'а')
+        bigovane_instrument_answer = check_select_answer(6, exam_title, student_class,
+                                                         q6_bigovane_instrument_answer, 'б')
+        drilling_instrument_answer = check_select_answer(6, exam_title, student_class,
+                                                         q6_drilling_instrument_answer, 'в')
+        cut_instrument_answer = check_select_answer(6, exam_title, student_class,
+                                                    q6_cut_instrument_answer, 'г')
+
+        if drawing_instrument_answer is True \
+                and bigovane_instrument_answer is True \
+                and drilling_instrument_answer is True \
+                and cut_instrument_answer is True:
+            question_answer, points, question_type = get_qst_answer_info(6, exam_title,
+                                                                         student_class)
+            final_points += points
+
+        """Проверка на въпрос 7"""
+        stocks_list = check_multiple_choice_answer(7, exam_title,
+                                                   student_class, q7_stocks_list)
+        if stocks_list is True:
+            question_answer, points, question_type = get_qst_answer_info(7, exam_title,
+                                                                         student_class)
+            final_points += points
+
+        """Проверка на въпрос 8"""
+        correct_word = check_multiple_choice_answer(8, exam_title,
+                                                    student_class, q8_correct_word)
+        if correct_word is True:
+            question_answer, points, question_type = get_qst_answer_info(8, exam_title,
+                                                                         student_class)
+            final_points += points
+
+        final_mark_update(exam_title=exam_title,student_num=student_num,
+                          student_name=student_name, student_class=student_class,
+                          final_points=final_points)
+
+        flash(f"Вашите отговори бяха успешно записани. Очаквайте вашата оценка в сайта на МОН",
+              "success")
 
     return render_template('first_exam.html')
+
+
+@app.route(rule='/design_and_construction', methods=["GET", "POST"])
+def design_and_construction_page():
+    return render_template('design_and_construction.html')
 
 
 if __name__ == "__main__":
